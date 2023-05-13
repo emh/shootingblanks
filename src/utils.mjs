efe33b418fd2728bbcdb8d1656b4300fc739cd81
Evan Haveman <evanhaveman@gmail.com>
2023-05-13 11:32:14 -0700

    derp

--- src/utils.mjs
+++ src/utils.mjs
@@ -5,7 +5,7 @@ export const loadFile = (file) => fetch(file).then((response) => response.text()
 export const key = () => {
     const d = new Date(); // local time
 
-    return `${d.getFullYear()}-${String(d.getMonth() + 1).padStart(2, '0')}-${String(d.getDate() + 2).padStart(2, '0')}`;
+    return `${d.getFullYear()}-${String(d.getMonth() + 1).padStart(2, '0')}-${String(d.getDate()).padStart(2, '0')}`;
 }
 
 export const getHistory = () => JSON.parse(localStorage.getItem('shootingblanks')) ?? {};
